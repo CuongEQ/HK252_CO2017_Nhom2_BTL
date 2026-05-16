e94c82cba5fc1db2959a05c71ce295e3ab76ddf7
Nguyen Minh Cuong - 2410432 <minhcuonganloca@gmail.com>
2026-05-16 19:33:06 +0000

    Refactor syscall handling. Implement Memory Mapping Syscall

--- src/syscall.c
+++ src/syscall.c
@@ -11,36 +11,39 @@
 #include "syscall.h"
 #include "common.h"
 
-#define __SYSCALL(nr, sym) extern int __##sym(struct krnl_t*, uint32_t,struct sc_regs*);
+#define __SYSCALL(nr, sym) extern int __##sym(struct krnl_t *, uint32_t, struct sc_regs *);
 #include "syscalltbl.lst"
-#undef  __SYSCALL
+#undef __SYSCALL
 
 /*
  * The sys_call_table[] is used for system calls, but to know the system
  * call address.
  */
 #define __SYSCALL(nr, sym) #nr "-" #sym,
-const char* sys_call_table[] = {
+const char *sys_call_table[] = {
 #include "syscalltbl.lst"
 };
-#undef  __SYSCALL
-const int syscall_table_size = sizeof(sys_call_table)/sizeof(char*);
+#undef __SYSCALL
+const int syscall_table_size = sizeof(sys_call_table) / sizeof(char *);
 
 int __sys_ni_syscall(struct krnl_t *krnl, struct sc_regs *regs)
 {
    /*
-    * DUMMY systemcall
+    * DUMMY systemcall for unimplemented system calls, it should never be called in normal execution.
     */
 
    return 0;
 }
 
-#define __SYSCALL(nr, sym) case nr: return __##sym(krnl,pid,regs);
-int _syscall(struct krnl_t *krnl, uint32_t pid, uint32_t nr, struct sc_regs* regs)
+#define __SYSCALL(nr, sym) \
+   case nr:                \
+      return __##sym(krnl, pid, regs);
+int _syscall(struct krnl_t *krnl, uint32_t pid, uint32_t nr, struct sc_regs *regs)
 {
-	switch (nr) {
-	#include "syscalltbl.lst"
-	default: return __sys_ni_syscall(krnl, regs);
-	}
+   switch (nr)
+   {
+#include "syscalltbl.lst"
+   default:
+      return __sys_ni_syscall(krnl, regs);
+   }
 };
-
