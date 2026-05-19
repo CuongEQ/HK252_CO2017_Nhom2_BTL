bd26597c13665d230fe07ad0980dd35454fcfd25
Nguyen Minh Cuong - 2410432 <minhcuonganloca@gmail.com>
2026-05-19 08:11:52 +0000

    GNU Style compliance

--- src/sys_listsyscall.c
+++ src/sys_listsyscall.c
@@ -10,7 +10,8 @@
 
 #include "syscall.h"
 
-int __sys_listsyscall(struct krnl_t *krnl, uint32_t pid, struct sc_regs* reg)
+int
+__sys_listsyscall(struct krnl_t *krnl, uint32_t pid, struct sc_regs* reg)
 {
    for (int i = 0; i < syscall_table_size; i++)
        printf("%s\n",sys_call_table[i]); 
