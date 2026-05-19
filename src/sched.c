207c7005c7ad433b50e06c3e6fb5dba2fc7ee001
Luu Bao Trang <luubaotrang20@gmail.com>
2026-05-19 15:21:09 +0700

    fix: missing put_proc

--- src/sched.c
+++ src/sched.c
@@ -11,13 +11,11 @@
 #include "queue.h"
 #include "sched.h"
 #include <pthread.h>
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 static struct queue_t ready_queue;
 static struct queue_t run_queue;
-// static pthread_mutex_t queue_lock;
 pthread_mutex_t queue_lock;
 
 static struct queue_t running_list;
@@ -27,26 +25,30 @@ static int slot[MAX_PRIO];
 #endif
 
 int queue_empty(void) {
+  int is_empty = 1;
+  pthread_mutex_lock(&queue_lock);
+  
 #ifdef MLQ_SCHED
-  unsigned long prio;
-  for (prio = 0; prio < MAX_PRIO; prio++) {
+  for (unsigned long prio = 0; prio < MAX_PRIO; prio++) {
     if (!empty(&mlq_ready_queue[prio])) {
-      return 0; 
+      is_empty = 0; // FIX: Trả về 0 (không rỗng) thay vì -1
+      break;
     }
   }
-  return 1; 
 #else
-  return (empty(&ready_queue) && empty(&run_queue));
+  is_empty = (empty(&ready_queue) && empty(&run_queue));
 #endif
+
+  pthread_mutex_unlock(&queue_lock);
+  return is_empty;
 }
 
 void init_scheduler(void) {
 #ifdef MLQ_SCHED
   int i;
-
   for (i = 0; i < MAX_PRIO; i++) {
     mlq_ready_queue[i].size = 0;
-    slot[i] = MAX_PRIO - i;
+    slot[i] = MAX_PRIO - i; 
   }
 #endif
   ready_queue.size = 0;
@@ -56,12 +58,10 @@ void init_scheduler(void) {
 }
 
 #ifdef MLQ_SCHED
-/*
- * Stateful design for routine calling
+/* * Stateful design for routine calling
  * based on the priority and our MLQ policy
  * We implement stateful here using transition technique
- * State representation   prio = 0 .. MAX_PRIO, curr_slot = 0..(MAX_PRIO -
- * prio)
+ * State representation   prio = 0 .. MAX_PRIO, curr_slot = 0..(MAX_PRIO - prio)
  */
 struct pcb_t *get_mlq_proc(void) {
   struct pcb_t *proc = NULL;
@@ -75,6 +75,7 @@ struct pcb_t *get_mlq_proc(void) {
       goto found;
     }
   }
+  
   /* !found -> reset slot and loop again */
   for (int i = 0; i < MAX_PRIO; i++)
     slot[i] = MAX_PRIO - i;
@@ -86,7 +87,8 @@ struct pcb_t *get_mlq_proc(void) {
       goto found;
     }
   }
-  found:
+  
+found:
   if (proc != NULL)
     enqueue(&running_list, proc);
 
@@ -117,7 +119,7 @@ void put_proc(struct pcb_t *proc) {
   put_mlq_proc(proc);
 }
 
-void add_proc(struct pcb_t *proc) {
+void add_proc(struct pcb_t * proc) {
   add_mlq_proc(proc);
 }
 
@@ -133,7 +135,6 @@ struct pcb_t *get_proc(void) {
       enqueue(&ready_queue, dequeue(&run_queue));
     }
   }
-
   if (!empty(&ready_queue)) {
     proc = dequeue(&ready_queue);
     if (proc != NULL) 
@@ -141,7 +142,6 @@ struct pcb_t *get_proc(void) {
   }
 
   pthread_mutex_unlock(&queue_lock);
-
   return proc;
 }
 
